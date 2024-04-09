3f0e5d5a4aae11c9d6f77dd111baee4dc05f3583
jpcchaves <joao.chaves@liferay.com>
2024-04-09 11:06:54 -0300

    feat: add tax number and email not available exceptions

--- core/src/main/java/com/cleanarch/core/exception/enums/ErrorCodeEnum.java
+++ core/src/main/java/com/cleanarch/core/exception/enums/ErrorCodeEnum.java
@@ -2,6 +2,8 @@ package com.cleanarch.core.exception.enums;
 
 public enum ErrorCodeEnum {
     ON0001("Invalid tax number", "ON-0001"),
+    ON0002("Tax number not available", "ON-0002"),
+    ON0003("Email not available", "ON-0003"),
     TR0001("The operation could not be completed due to insufficient authorization", "TR-0001"),
     TR0002("Insufficient balance", "TR-0002"),
     TRP0001("Invalid pin", "TRP-0001");
