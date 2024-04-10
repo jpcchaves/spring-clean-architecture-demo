777df663783fbe3f05c26a758617db4a7363a9ce
jpcchaves <joao.chaves@liferay.com>
2024-04-10 16:50:06 -0300

    feat: add transaction pin usecase and impl

---
+++ application/src/main/java/com/cleanarch/application/usecaseimpl/TransactionPinValidateUseCaseImpl.java
@@ -0,0 +1,11 @@
+package com.cleanarch.application.usecaseimpl;
+
+import com.cleanarch.core.domain.TransactionPin;
+import com.cleanarch.usecase.TransactionPinValidateUseCase;
+
+public class TransactionPinValidateUseCaseImpl implements TransactionPinValidateUseCase {
+    @Override
+    public Boolean validate(TransactionPin transactionPin) {
+        return null;
+    }
+}
