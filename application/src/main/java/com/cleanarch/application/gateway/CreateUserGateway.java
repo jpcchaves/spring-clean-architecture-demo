938cb6ea526066f8a804c383d7198007ddccaad1
jpcchaves <joao.chaves@liferay.com>
2024-04-10 16:52:03 -0300

    refactor: adjust create user usecase to match new relation between transaction pin and wallet

--- application/src/main/java/com/cleanarch/application/gateway/CreateUserGateway.java
+++ application/src/main/java/com/cleanarch/application/gateway/CreateUserGateway.java
@@ -5,5 +5,5 @@ import com.cleanarch.core.domain.User;
 import com.cleanarch.core.domain.Wallet;
 
 public interface CreateUserGateway {
-    boolean create(User user, Wallet wallet, TransactionPin transactionPin);
+    boolean create(User user, Wallet wallet);
 }
