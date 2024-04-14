47b1605cc943c35cbacc4e8ce19be32dde05c532
João Chaves <joao.chaves@liferay.com>
2024-04-13 21:12:33 -0300

    feat: add getters, setters, equals and hashcode that were missing

--- infrastructure/src/main/java/com/cleanarch/infrastructure/persistence/entity/WalletEntity.java
+++ infrastructure/src/main/java/com/cleanarch/infrastructure/persistence/entity/WalletEntity.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Entity
 @Table(name = "wallets")
@@ -62,5 +63,85 @@ public class WalletEntity {
         this.userId = userId;
         this.transactionPinId = transactionPinId;
         this.createdAt = LocalDateTime.now();
+    }
+
+    public WalletEntity(BigDecimal balance,
+                        UserEntity userEntity,
+                        TransactionPinEntity transactionPinEntity,
+                        LocalDateTime createdAt,
+                        LocalDateTime updatedAt) {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    public UserEntity getUserId() {
+        return userId;
+    }
+
+    public void setUserId(UserEntity userId) {
+        this.userId = userId;
+    }
+
+    public TransactionPinEntity getTransactionPinId() {
+        return transactionPinId;
+    }
+
+    public void setTransactionPinId(TransactionPinEntity transactionPinId) {
+        this.transactionPinId = transactionPinId;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        WalletEntity that = (WalletEntity) o;
+        return Objects.equals(id, that.id) && Objects.equals(balance, that.balance) && Objects.equals(userId, that.userId) && Objects.equals(transactionPinId, that.transactionPinId) && Objects.equals(createdAt, that.createdAt) && Objects.equals(updatedAt, that.updatedAt);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, balance, userId, transactionPinId, createdAt, updatedAt);
+    }
+
+    @Override
+    public String toString() {
+        return "WalletEntity{" +
+            "id=" + id +
+            ", balance=" + balance +
+            ", userId=" + userId +
+            ", transactionPinId=" + transactionPinId +
+            ", createdAt=" + createdAt +
+            ", updatedAt=" + updatedAt +
+            '}';
     }
 }
