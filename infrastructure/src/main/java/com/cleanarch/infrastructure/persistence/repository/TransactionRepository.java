4c3d109e5c36a8c0f200f01555abd6780de54931
João Chaves <joao.chaves@liferay.com>
2024-04-13 21:13:11 -0300

    fix: adjust transaction pin jpa entity wrong class inside generics

--- infrastructure/src/main/java/com/cleanarch/infrastructure/persistence/repository/TransactionRepository.java
+++ infrastructure/src/main/java/com/cleanarch/infrastructure/persistence/repository/TransactionRepository.java
@@ -1,8 +1,9 @@
 package com.cleanarch.infrastructure.persistence.repository;
 
+import com.cleanarch.infrastructure.persistence.entity.TransactionEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TransactionRepository extends JpaRepository<TransactionRepository, Long> {
+public interface TransactionRepository extends JpaRepository<TransactionEntity, Long> {
 }
